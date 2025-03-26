9b907bdd08007b621482bc326ee75f51149dd4f2
Alejandra <alejandrasandoval4321@gmail.com>
2025-03-26 10:59:53 -0600

    anotaciones de datos

--- PracticaMVC/Models/marcas.cs
+++ PracticaMVC/Models/marcas.cs
@@ -5,8 +5,14 @@ namespace PracticaMVC.Models
     public class marcas
     {
         [Key]
+        [Display(Name ="ID de Marca")]
         public int id_marcas { get; set; }
+
+        [Display(Name = "Nombre de la Marca")]
+        [Required(ErrorMessage = "El nombre de la marca NO es opcional")]
         public string? nombre_marca { get; set; }
+
+        [Display(Name = "Estado")]
         public string? estados { get; set; }
 
 
